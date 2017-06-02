1d0da280f2e9633fd3bf8cb9f6b574b185eeb7a4
Jacelly <Jacelly@espol.edu.ec>
2017-06-01 22:58:25 -0500

    Auto stash before merge of "master" and "origin/master"

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -5,17 +5,33 @@
  */
 package proyecto;
 
+import inicio_sesion.InicioSesion;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Julio Alvia
  */
-public class Proyecto {
+public class Proyecto extends Application{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        InicioSesion root=new InicioSesion(primaryStage);
+        root.menuPrincipal(primaryStage);
+        Scene scene=new Scene(root.getRoot(),500,300); 
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     
 }
