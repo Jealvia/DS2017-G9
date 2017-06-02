bbe48a94ce4141101b235a945f1fc398cf315dfb
Unknown <jealvia@espol.edu.ec>
2017-06-01 21:37:12 -0500

    Avance
    Algunas clases, metodos y modelos previos al csv

---
+++ Proyecto/src/Modelo/Persistencia.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Julio Alvia
+ */
+public class Persistencia {
+    
+    
+    public static HashMap<String, Usuario> leerUsuarios(){
+    
+    }
+    
+    public static boolean agregarAdministrador(){
+    
+    return true;
+            }
+}
---
+++ Proyecto/src/Modelo/Restaurante.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Julio Alvia
+ */
+public class Restaurante {
+    private String nombre,descripcion,telefono;
+    
+    public Restaurante(){}
+}
