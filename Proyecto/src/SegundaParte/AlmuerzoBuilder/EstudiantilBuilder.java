c6def1189258694c840f1f48e4bba3bc27928667
KarolNicole <knle21597@gmail.com>
2017-08-11 12:11:07 -0500

    Más Builder

--- Proyecto/src/SegundaParte/AlmuerzoBuilder/EstudiantilBuilder.java
+++ Proyecto/src/SegundaParte/AlmuerzoBuilder/EstudiantilBuilder.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package AlmuerzoBuilder;
+package SegundaParte.AlmuerzoBuilder;
 
 /**
  *
  * @author FAUSTO LOPEZ
  */
-public class LunchDirector {
+public class EstudiantilBuilder {
     
 }
