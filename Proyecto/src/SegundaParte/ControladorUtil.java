47d9c6914a7652153427945f30a770ac1db2c4d0
Unknown <jealvia@espol.edu.ec>
2017-08-11 12:13:20 -0500

    Auto stash before merge of "master" and "origin/master"

--- Proyecto/src/SegundaParte/ControladorUtil.java
+++ Proyecto/src/SegundaParte/ControladorUtil.java
@@ -7,36 +7,27 @@
  * and open the template in the editor.
  
  */
- 
 package SegundaParte;
- 
 
- 
 import java.util.ArrayList;
- 
 
- 
 /**
- 
  *
- 
+ *
+ *
  * @author Julio Alvia
- 
+ *
  */
- 
 public class ControladorUtil {
- 
+
     private ArrayList<Restaurante> listaRestaurantes;
- 
+
     private ArrayList<Restaurante> listaUsuarios;
- 
-    
- 
-    public void llenarListaRestaurantes(){}
- 
-    public void llenarListaUsuarioss(){}
- 
-    
- 
+
+    public void llenarListaRestaurantes() {
+    }
+
+    public void llenarListaUsuarioss() {
+    }
+
 }
- 
